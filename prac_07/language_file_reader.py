482cf6265c57391a1b5d905180b645da67c0b6da
천영후 <younghoo.cheon@my.jcu.edu.au>
2025-07-08 16:14:48 +0800

    Add pseudocode for programming language data structure and tests.

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -1,5 +1,46 @@
 """
+function main():
+    languages = []
+    open file 'languages.csv' for reading as in_file
+    read and ignore header line
+    for each line in in_file:
+        split line by ','
+        set reflection to true if third part is 'Yes'
+        create language object with name, typing, reflection, and year
+        add language to languages list
+    close in_file
 
+    for each language in languages:
+        print language
+
+main()
+
+function using_csv():
+    open file 'languages.csv' for reading as in_file
+    read and ignore header line
+    create csv reader from in_file
+    for each row in reader:
+        print row
+    close in_file
+
+function using_namedtuple():
+    open file 'languages.csv' for reading as in_file
+    read header line and split into field names
+    print field names
+    define namedtuple 'language' with fields: name, typing, reflection, year
+    create csv reader from in_file
+    for each row in reader:
+        create language from row using _make
+        print language using repr
+    close in_file
+
+function using_csv_namedtuple():
+    define namedtuple 'language' with fields: name, typing, reflection, year
+    open file 'languages.csv' for reading as in_file
+    read and ignore header line
+    for each language in map _make over csv reader of in_file:
+        print language.name + ' was released in ' + language.year
+        print repr(language)
 """
 import csv
 from collections import namedtuple
