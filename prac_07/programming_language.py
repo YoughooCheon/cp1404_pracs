5fecf8909bc8fd8121602d84b0f5bd77b429e55a
천영후 <younghoo.cheon@my.jcu.edu.au>
2025-07-08 16:12:42 +0800

    Add pseudocode for programming language data structure and tests.

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -1,5 +1,40 @@
 """
+class ProgrammingLanguage
 
+function create_programming_language(name, typing, reflection, year):
+    language = {
+        "name": name,
+        "typing": typing,
+        "reflection": reflection,
+        "year": year
+    }
+    return language
+
+function get_language_string(language):
+    return language["name"] + ", " + language["typing"] + " typing, reflection=" + str(language["reflection"]) + ", first appeared in " + str(language["year"])
+
+function is_dynamic(language):
+    return language["typing"] == "dynamic"
+
+function run_tests():
+    ruby = create_programming_language("ruby", "dynamic", true, 1995)
+    python = create_programming_language("python", "dynamic", true, 1991)
+    visual_basic = create_programming_language("visual basic", "static", false, 1991)
+
+    languages = [ruby, python, visual_basic]
+
+    print get_language_string(python)
+
+    print "the dynamically typed languages are:"
+
+    for each language in languages:
+        if is_dynamic(language):
+            print language["name"]
+
+function main():
+    run_tests()
+
+main()
 """
 class ProgrammingLanguage:
     """Represent information about a programming language."""
