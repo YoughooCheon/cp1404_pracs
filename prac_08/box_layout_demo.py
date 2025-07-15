07972f11cd038a1caeb9a275cb0462be8707352a
천영후 <younghoo.cheon@my.jcu.edu.au>
2025-07-15 10:15:56 +0800

    Add basic BoxLayout app with greeting and clear functionality.
    Implemented BoxLayoutDemo Kivy app with input handling and UI updates.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-
 class BoxLayoutDemo(App):
     def build(self):
         self.root = Builder.load_file('box_layout.kv')
@@ -14,7 +13,6 @@ class BoxLayoutDemo(App):
     def handle_clear(self):
         self.root.ids.input_name.text = ''
         self.root.ids.output_label.text = ''
-
 
 if __name__ == '__main__':
     BoxLayoutDemo().run()
